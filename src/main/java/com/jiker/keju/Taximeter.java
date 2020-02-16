9d2e6d0289ca58c2597e4956f0a44eadb49efcaa
Linesh <linesh.simpcity@gmail.com>
2020-02-16 19:57:25 +0800

    ♻️refactor: revert logics until I feel good

--- src/main/java/com/jiker/keju/Taximeter.java
+++ src/main/java/com/jiker/keju/Taximeter.java
@@ -1,14 +1,15 @@
 package com.jiker.keju;
 
 public class Taximeter {
-    public static final double CHARGES_PER_MINUTE_WAITING = 0.25;
-    public static final int LONG_DISTANCE_BASE = 8;
     public static final int FREE_KILOMETERS = 2;
+    public static final int LONG_DISTANCE_BASE_KILOMETERS = 8;
+    public static final int CHARGES_BASIC_PRICE = 6;
     public static final double CHARGES_PER_KILOMETER = 0.8;
+    public static final double CHARGES_PER_MINUTE_WAITING = 0.25;
+    public static final double CHARGES_PERCENT_OF_LONG_DISTANCE_KILOMETERS = 0.5;
+
     private final int mileage;
     private final int waitingMinutes;
-
-    public static final int BASIC_PRICE = 6;
 
     public Taximeter(int mileage, int waitingMinutes) {
         this.mileage = mileage;
@@ -24,7 +25,7 @@ public class Taximeter {
     }
 
     public int calculatePrice() {
-        return (int) Math.round(BASIC_PRICE + mileageCharges() + longDistanceFare() + waitingCharges());
+        return (int) Math.round(CHARGES_BASIC_PRICE + mileageCharges() + longDistanceFare() + waitingCharges());
     }
 
     private double waitingCharges() {
@@ -32,10 +33,18 @@ public class Taximeter {
     }
 
     private double longDistanceFare() {
-        return (mileage > LONG_DISTANCE_BASE) ? (mileage - LONG_DISTANCE_BASE) * CHARGES_PER_KILOMETER * 0.5 : 0;
+        if (mileage > LONG_DISTANCE_BASE_KILOMETERS) {
+            return (mileage - LONG_DISTANCE_BASE_KILOMETERS) * CHARGES_PER_KILOMETER * CHARGES_PERCENT_OF_LONG_DISTANCE_KILOMETERS;
+        }
+
+        return 0;
     }
 
     private double mileageCharges() {
-        return mileage > FREE_KILOMETERS ? (mileage - FREE_KILOMETERS) * CHARGES_PER_KILOMETER : 0;
+        if (mileage > FREE_KILOMETERS) {
+            return (mileage - FREE_KILOMETERS) * CHARGES_PER_KILOMETER;
+        }
+
+        return 0;
     }
 }
