5ac656b75f790071ca7b4385105136ccff732ab4
Linesh <linesh.simpcity@gmail.com>
2020-02-16 20:27:57 +0800

    ♻️refactor: remove test so that test file wouldn't exceed 50 lines

--- src/test/java/com/jiker/keju/TaximeterTest.java
+++ src/test/java/com/jiker/keju/TaximeterTest.java
@@ -35,13 +35,6 @@ public class TaximeterTest {
     }
 
     @Test
-    public void should_parse_test_input_and_give_result() {
-        String result = TaximeterPrinter.process("1公里,等待0分钟");
-
-        assertThat(result, is("收费6元"));
-    }
-
-    @Test
     public void should_parse_multiple_lines_of_test_input_and_give_result() {
         String result = TaximeterPrinter.process("1公里,等待0分钟" + "\n" + "3公里,等待0分钟\n" + "10公里,等待0分钟\n" + "2公里,等待3分钟");
 
