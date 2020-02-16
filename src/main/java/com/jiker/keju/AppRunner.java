04d0c382f48b29bb3463ba7188a385e36e4a59a1
Linesh <linesh.simpcity@gmail.com>
2020-02-16 19:09:24 +0800

    🔧chore: initial setup - read file from txt

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -1,15 +1,29 @@
 package com.jiker.keju;
 
+import java.io.*;
+
 public class AppRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /*TODO
           args[0]为resources下的测试数据文件名。
           读取测试数据文件作为你程序的输入参数，
           并将结果保存到receipt变量中。
          */
-        String testDataFile = args[0];
+        String testDataFile = "src/main/resources/testData.txt";
+        final FileInputStream fileInputStream = new FileInputStream(testDataFile);
+        final InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+        StringBuffer result = new StringBuffer();
+        String text = null;
+        while((text = bufferedReader.readLine()) != null){
+            result.append(text + "\n");
+        }
+        String input = result.toString();
+
         String receipt = "";
+        System.out.println(input);
         System.out.println(receipt);
     }
 }
