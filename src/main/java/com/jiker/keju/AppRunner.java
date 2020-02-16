86182d8281c3c921e612597e59b3911d12a7815f
Linesh <linesh.simpcity@gmail.com>
2020-02-16 20:31:05 +0800

    ♻️refactor: extract readFileAsText()

--- src/main/java/com/jiker/keju/AppRunner.java
+++ src/main/java/com/jiker/keju/AppRunner.java
@@ -11,18 +11,23 @@ public class AppRunner {
           并将结果保存到receipt变量中。
          */
         String testDataFile = "src/main/resources/testData.txt";
-        final FileInputStream fileInputStream = new FileInputStream(testDataFile);
-        final InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
-
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        StringBuffer result = new StringBuffer();
-        String text = "";
-        while((text = bufferedReader.readLine()) != null){
-            result.append(text + "\n");
-        }
-        String input = result.toString();
+        String input = readFileAsString(testDataFile);
 
         String receipt = TaximeterPrinter.process(input);
         System.out.println(receipt);
+    }
+
+    private static String readFileAsString(String testDataFile) throws IOException {
+        final FileInputStream fileInputStream = new FileInputStream(testDataFile);
+        final InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        StringBuilder result = new StringBuilder();
+        String text = "";
+        while((text = bufferedReader.readLine()) != null){
+            result.append(text).append("\n");
+        }
+
+        return result.toString();
     }
 }
